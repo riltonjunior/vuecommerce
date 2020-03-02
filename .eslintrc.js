1440e83231a8fbaaa27a086b1c613220805e5919
Rilton Junior <riltonjunior@gmail.com>
2020-03-02 08:38:11 -0300

    First commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,13 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "vue/html-quotes": "double",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   }
-}
+};
